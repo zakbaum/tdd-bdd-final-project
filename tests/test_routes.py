2c3d3665b9ccc18287ee31151b79ece95c3cd5ba
zakbaum <zachbaum.zb@gmail.com>
2024-10-01 12:09:26 -0400

    working

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -28,6 +28,7 @@ import os
 import logging
 from decimal import Decimal
 from unittest import TestCase
+from urllib.parse import quote_plus
 from service import app
 from service.common import status
 from service.models import db, init_db, Product
@@ -197,76 +198,133 @@ class TestProductRoutes(TestCase):
         updated_product = response.get_json()
         self.assertEqual(updated_product["description"], "unknown")
 
-    def test_delete_a_product(self):
-        """It should Delete a Product"""
-        product = ProductFactory()
-        product.create()
-        self.assertEqual(len(Product.all()), 1)
-        # delete the product and make sure it isn't in the database
-        product.delete()
-        self.assertEqual(len(Product.all()), 0)
-
-    #def test_delete_product(self):
+    # def test_delete_a_product(self):
     #    """It should Delete a Product"""
-    #    products = self._create_products(5)
-    #    product_count = self.get_product_count()
-    #    test_product = products[0]
-    #    response = self.client.delete(f"{BASE_URL}/{test_product.id}")
-    #    self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    #    self.assertEqual(len(response.data), 0)
-    #    # make sure they are deleted
-    #    response = self.client.get(f"{BASE_URL}/{test_product.id}")
-    #    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    #    new_count = self.get_product_count()
-    #    self.assertEqual(new_count, product_count - 1)
+    #    product = ProductFactory()
+    #    product.create()
+    #    self.assertEqual(len(Product.all()), 1)
+    #    # delete the product and make sure it isn't in the database
+    #    product.delete()
+    #    self.assertEqual(len(Product.all()), 0)
 
-    def test_list_all_products(self):
-        """It should List all Products in the database"""
-        products = Product.all()
-        self.assertEqual(products, [])
-        # Create 5 Products
-        for _ in range(5):
-            product = ProductFactory()
-            product.create()
-        # See if we get back 5 products
-        products = Product.all()
-        self.assertEqual(len(products), 5)
+    def test_delete_product(self):
+        """It should Delete a Product"""
+        products = self._create_products(5)
+        product_count = self.get_product_count()
+        test_product = products[0]
+        response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # make sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        new_count = self.get_product_count()
+        self.assertEqual(new_count, product_count - 1)
 
-    def test_find_by_name(self):
-        """It should Find a Product by Name"""
-        products = ProductFactory.create_batch(5)
-        for product in products:
-            product.create()
-        name = products[0].name
-        count = len([product for product in products if product.name == name])
-        found = Product.find_by_name(name)
-        self.assertEqual(found.count(), count)
-        for product in found:
-            self.assertEqual(product.name, name)
+    # def test_list_all_products(self):
+    #    """It should List all Products in the database"""
+    #    products = Product.all()
+    #    self.assertEqual(products, [])
+    #    # Create 5 Products
+    #    for _ in range(5):
+    #        product = ProductFactory()
+    #        product.create()
+    #    # See if we get back 5 products
+    #    products = Product.all()
+    #    self.assertEqual(len(products), 5)
+    #
 
-    def test_find_by_availability(self):
-        """It should Find Products by Availability"""
-        products = ProductFactory.create_batch(10)
-        for product in products:
-            product.create()
-        available = products[0].available
-        count = len([product for product in products if product.available == available])
-        found = Product.find_by_availability(available)
-        self.assertEqual(found.count(), count)
-        for product in found:
-            self.assertEqual(product.available, available)
+    def test_get_product_list(self):
+        """It should Get a list of Products"""
+        self._create_products(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
 
-    def test_find_by_category(self):
-        """It should Find Products by Category"""
-        products = ProductFactory.create_batch(10)
-        for product in products:
-            product.create()
+    def test_query_by_name(self):
+        """It should Query Products by name"""
+        products = self._create_products(5)
+        test_name = products[0].name
+        name_count = len([product for product in products if product.name == test_name])
+        response = self.client.get(
+            BASE_URL, query_string=f"name={quote_plus(test_name)}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), name_count)
+        # check the data just to be sure
+        for product in data:
+            self.assertEqual(product["name"], test_name)
+
+    # def test_find_by_name(self):
+    #    """It should Find a Product by Name"""
+    #    products = ProductFactory.create_batch(5)
+    #    for product in products:
+    #        product.create()
+    #    name = products[0].name
+    #    count = len([product for product in products if product.name == name])
+    #    found = Product.find_by_name(name)
+    #    self.assertEqual(found.count(), count)
+    #    for product in found:
+    #        self.assertEqual(product.name, name)
+
+    # def test_find_by_availability(self):
+    #    """It should Find Products by Availability"""
+    #    products = ProductFactory.create_batch(10)
+    #    for product in products:
+    #        product.create()
+    #    available = products[0].available
+    #    count = len([product for product in products if product.available == available])
+    #    found = Product.find_by_availability(available)
+    #    self.assertEqual(found.count(), count)
+    #    for product in found:
+    #        self.assertEqual(product.available, available)
+
+    def test_query_by_availability(self):
+        """It should Query Products by availability"""
+        products = self._create_products(10)
+        available_products = [product for product in products if product.available is True]
+        available_count = len(available_products)
+        # test for available
+        response = self.client.get(
+            BASE_URL, query_string="available=true"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), available_count)
+        # check the data just to be sure
+        for product in data:
+            self.assertEqual(product["available"], True)
+
+    def test_query_by_category(self):
+        """It should Query Products by category"""
+        products = self._create_products(10)
         category = products[0].category
-        count = len([product for product in products if product.category == category])
-        found = Product.find_by_category(category)
-        self.assertEqual(found.count(), count)
-        for product in found:
-            self.assertEqual(product.category, category)
+        found = [product for product in products if product.category == category]
+        found_count = len(found)
+        logging.debug("Found Products [%d] %s", found_count, found)
+
+        # test for available
+        response = self.client.get(BASE_URL, query_string=f"category={category.name}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), found_count)
+        # check the data just to be sure
+        for product in data:
+            self.assertEqual(product["category"], category.name)
+
+    # def test_find_by_category(self):
+    #    """It should Find Products by Category"""
+    #    products = ProductFactory.create_batch(10)
+    #    for product in products:
+    #        product.create()
+    #    category = products[0].category
+    #    count = len([product for product in products if product.category == category])
+    #    found = Product.find_by_category(category)
+    #    self.assertEqual(found.count(), count)
+    #    for product in found:
+    #        self.assertEqual(product.category, category)
 
     ######################################################################
     # Utility functions
